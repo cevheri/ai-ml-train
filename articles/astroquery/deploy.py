6dd5c77e52cbcb6429e543c7d511600d83829c4e
cevheri <cevheribozoglan@gmail.com>
2024-07-23 23:10:28 +0300

    fix deploy script

--- articles/astroquery/deploy.py
+++ articles/astroquery/deploy.py
@@ -1,10 +1,17 @@
 import requests
 
-MEDIUM_USER_ID = "cevheri"
+# Create a new token in https://medium.com/me/settings/ > security and app > integration tokens
+# get userId in https://api.medium.com/v1/me
+# curl -H "Authorization: Bearer TOKEN" -H "Content-Type: application/json" -H "Accept: application/json" https://api.medium.com/v1/me
+# copy the id
+
+
+
+MEDIUM_USER_ID = "USERID"
 MEDIUM_API_URL = f"https://api.medium.com/v1/users/{MEDIUM_USER_ID}/posts"
-MEDIUM_TOKEN = "2cd0db9592306382da5142893dfa85f0eb4260b88d70b22d37728b9a12a1521b9"
+MEDIUM_TOKEN = "TOKEN"
 TITLE = "Gaia dataset and queries with ADQL (Astronomical Data Query Language)"
-FILE_NAME = "ADQL.ipynb"
+FILE_NAME = "ADQL.md"
 
 
 def main():
